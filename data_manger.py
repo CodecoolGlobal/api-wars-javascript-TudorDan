02fe140d750da2f427f35448470ad6737b141658
TudorDan <tudor.ist@gmail.com>
2020-04-16 14:24:29 +0300

    Add registration functionality

---
+++ data_manger.py
@@ -0,0 +1,36 @@
+import database_common
+import bcrypt
+
+
+def hash_password(plain_text_password):
+    # By using bcrypt, the salt is saved into the hash itself
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
+
+
+@database_common.connection_handler
+def check_user(cursor, username):
+    cursor.execute("""
+                    SELECT username FROM users
+                    WHERE username = %(username)s;
+                    """,
+                   {'username': username})
+    user_dict = cursor.fetchone()
+    if user_dict is None:
+        return True
+    return False
+
+
+@database_common.connection_handler
+def add_new_user(cursor, username, hashed_password):
+    cursor.execute("""
+                    INSERT INTO users(username, password)
+                    VALUES (%(username)s, %(hashed_password)s);
+                    """,
+                   {'username': username,
+                    'hashed_password': hashed_password})
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from functools import wraps
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, request, session
+
+import data_manger
 
 app = Flask(__name__)
 app.secret_key = 'Dromihete'
@@ -24,7 +26,20 @@ def index():
 
 @app.route('/registration', methods=['GET', 'POST'])
 def add_new_user():
-    return render_template('add_new_user.html')
+    user_ok = True
+    if request.method == 'POST':
+        username = request.form['form-username']
+        password = request.form['form-password']
+
+        user_ok = data_manger.check_user(username)
+
+        if user_ok:
+            hashed_password = data_manger.hash_password(password)
+            data_manger.add_new_user(username, hashed_password)
+            return redirect(url_for('index'))
+    return render_template('add_new_user.html', user_ok=user_ok)
+
+
 
 
 if __name__ == '__main__':
