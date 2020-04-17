4acda973e73620b61ad85fa641d01f1cfdaf667b
TudorDan <tudor.ist@gmail.com>
2020-04-17 18:47:29 +0300

    Add modal for residents

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,9 @@
 let urlApi = 'http://swapi.dev/api/planets/?page=1';
-let prevUrl = '';
-let nextUrl = '';
+let prevUrl;
+let nextUrl;
 const nextButton = document.querySelector('#nextButton');
 const prevButton = document.querySelector('#prevButton');
+let allPersons = [];
 
 async function getPlanets(page) {
     let response = await fetch(page);
@@ -33,7 +34,8 @@ function displayPlanets(planets) {
             let residentsButton = 'No known residents';
             if (planet.residents.length > 0) {
                 residentsButton = `
-                <button type="button" class="btn btn-outline-secondary">${planet.residents.length} resident(s)</button>
+                <button type="button" class="btn btn-outline-secondary" 
+                onclick="showModal('${planet.url}')">${planet.residents.length} resident(s)</button>
                 `;
             }
             // Generate table
@@ -86,11 +88,79 @@ function prevClick() {
     }
 }
 
+function showModal(planet) {
+    console.log(allPersons);
+    let specificPlanetResidents = []
+    for (let person of allPersons) {
+        if (person.homeworld === planet) {
+            specificPlanetResidents.push(person)
+        }
+    }
+    let body = '';
+    for (let resident of specificPlanetResidents) {
+        body += `
+        <tr>
+            <td>${resident.name}</td>
+            <td>${resident.height / 100 + ' m'}</td>
+            <td>${resident.mass === 'unknown' ? 'unknown' : (resident.mass + ' kg')}</td>
+            <td>${resident.skin_color}</td>
+            <td>${resident.hair_color}</td>
+            <td>${resident.eye_color}</td>
+            <td>${resident.birth_year}</td>
+            <td>${resident.gender === 'unknown' ? 'unknown' : (resident.gender === 'female' ? '<i class="fas' +
+            ' fa-female" aria-hidden="true"></i>' : '<i class="fas fa-male" aria-hidden="true"></i>')}</td>
+        </tr>
+        `;
+    }
+    document.querySelector('.modal-body').innerHTML = `
+    <table class="table table-bordered table-light table-striped table-responsive-md">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Height</th>
+                <th>Mass</th>
+                <th>Skin Color</th>
+                <th>Hair Color</th>
+                <th>Eye Color</th>
+                <th>Birth Year</th>
+                <th>Gender</th>
+            </tr>
+        </thead>
+        <tbody>
+            ${body}
+        </tbody>
+    </table>
+    `;
+    $('#modalComponent').modal('show');
+}
+
+function getallPersons() {
+    let urlPersons = 'http://swapi.dev/api/people/?page=';
+    let temp = '';
+    for (let i = 1; i < 10; i++) {
+        temp = urlPersons.concat(`${i}`);
+        getOnePagePeople(temp);
+    }
+}
+
+function getOnePagePeople(urlPersons) {
+    fetch(urlPersons)
+        .then(data => data.json())
+        .then(data => {
+            let persons = data.results;
+            for (let person of persons) {
+                allPersons.push(person);
+            }
+        })
+}
+
 function init() {
     getPlanets(urlApi);
 
     nextButton.addEventListener('click', nextClick);
     prevButton.addEventListener('click', prevClick);
+
+    getallPersons();
 }
 
 init();
