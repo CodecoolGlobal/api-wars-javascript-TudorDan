62fa23032a27f0b5f56191c962e1ce0d46d793ea
TudorDan <tudor.ist@gmail.com>
2020-04-17 10:46:33 +0300

    Refactor pagination buttons

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,17 @@
+let urlApi = 'http://swapi.dev/api/planets/?page=1';
+let prevUrl = '';
+let nextUrl = '';
+const nextButton = document.querySelector('#nextButton');
+const prevButton = document.querySelector('#prevButton');
+
 async function getPlanets(page) {
     let response = await fetch(page);
     let data = await response.json();
 
-    setPagination(data.previous, data.next);
+    prevUrl = data.previous;
+    nextUrl = data.next;
+
+    setPagination(prevUrl, nextUrl);
     displayPlanets(data.results);
 }
 
@@ -13,22 +22,25 @@ function displayPlanets(planets) {
         let logged = table.dataset.loggedIn === 'True';
         tbody.innerHTML = '';
         for (let planet of planets) {
+            // Check if user login
             let voteButton = '';
             if (logged) {
                 voteButton = `
-                <td><button type="button" class="btn btn-light">Vote</button></td>
+                <td><button type="button" class="btn btn-outline-secondary">Vote</button></td>
                 `;
             }
+            // Check if residents number > 0
             let residentsButton = 'No known residents';
             if (planet.residents.length > 0) {
                 residentsButton = `
                 <button type="button" class="btn btn-outline-secondary">${planet.residents.length} resident(s)</button>
                 `;
             }
+            // Generate table
             tbody.innerHTML += `
             <tr>
                 <td>${planet.name}</td>
-                <td>${planet.diameter} km</td>
+                <td>${planet.diameter === 'unknown' ? 'unknown' : (planet.diameter + ' km')}</td>
                 <td>${planet.climate}</td>
                 <td>${planet.terrain}</td>
                 <td>${planet.surface_water === 'unknown' ? 'unknown' : (planet.surface_water + ' %')}</td>
@@ -42,44 +54,40 @@ function displayPlanets(planets) {
 }
 
 function setPagination(prevUrl, nextUrl) {
-    let prevButton = document.querySelector('#prevButton');
-    let nextButton = document.querySelector('#nextButton');
     if (prevUrl !== null) {
         prevButton.classList.remove('disabled');
-        prevButton.dataset.prevUrl = prevUrl;
+        prevButton.classList.remove('btn-secondary');
+        prevButton.classList.add('btn-primary');
     } else {
         prevButton.classList.add('disabled');
-        prevButton.dataset.prevUrl = 'null';
+        prevButton.classList.remove('btn-primary');
+        prevButton.classList.add('btn-secondary');
     }
     if (nextUrl !== null) {
         nextButton.classList.remove('disabled');
-        nextButton.dataset.nextUrl = nextUrl;
+        nextButton.classList.remove('btn-secondary');
+        nextButton.classList.add('btn-primary');
     } else {
         nextButton.classList.add('disabled');
-        nextButton.dataset.nextUrl = 'null';
+        nextButton.classList.remove('btn-primary');
+        nextButton.classList.add('btn-secondary');
     }
 }
 
 function nextClick() {
-    let nextButton = document.querySelector('#nextButton');
-    let nextUrl = nextButton.dataset.nextUrl;
     if (nextUrl !== 'null') {
         getPlanets(nextUrl);
     }
 }
 
 function prevClick() {
-    let prevButton = document.querySelector('#prevButton');
-    let prevUrl = prevButton.dataset.prevUrl;
     if (prevUrl !== 'null') {
         getPlanets(prevUrl);
     }
 }
 
 function init() {
-    getPlanets('http://swapi.dev/api/planets/?page=1');
-    let nextButton = document.querySelector('#nextButton');
-    let prevButton = document.querySelector('#prevButton');
+    getPlanets(urlApi);
 
     nextButton.addEventListener('click', nextClick);
     prevButton.addEventListener('click', prevClick);
